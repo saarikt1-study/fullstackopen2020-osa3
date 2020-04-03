d7a8bbf82fbfe54376f2ebfe88cc1a513c24cb42
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-04-03 12:25:41 +0200

    Finish exercise 3.2

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ let persons = [
   }
 ]
 
+app.get('/info', (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  )
+})
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
