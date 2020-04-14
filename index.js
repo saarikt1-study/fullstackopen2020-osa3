d408c52067145f496501af6bd69901039976b2a4
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-04-14 10:26:01 +0200

    Finish 3.13

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
@@ -13,24 +15,6 @@ morgan.token('content', (req, res) => {
     return JSON.stringify(req.body)
   }
 })
-
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
 
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
@@ -49,7 +33,9 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons.map(person => person.toJSON()))
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -85,7 +71,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
